cc7f43bb61cb28dbaa6baaddbaef8bdc1a3bfb73
Ayush2004 <36428932+Ayush2004@users.noreply.github.com>
2018-02-21 17:07:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/article-one', function (req, res) {
-  res.send("Article one");
+  res.send("Article-one");
 });
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
